840c5ee520d6680d4e3413f7b3433ee07fe868c4
Kytaihora Volodymyr <kytaihora.v@gmail.com>
2021-09-03 14:25:13 +0300

    Women pagev2 (#29)
    WomenPage

--- pages/landing_page.rb
+++ pages/landing_page.rb
@@ -2,5 +2,9 @@ class LandingPage < BasePage
   def sign_in_button
     browser.find_element(class: 'login')
   end
+  
+  def women_button
+    browser.find_element(xpath: "//a[@title='Women']")
+  end
 end
 

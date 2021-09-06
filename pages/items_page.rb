6a9976fb561b60b8fc8f54a7e73b5af8185c2ce6
AndrewKhukhrianskyi <55056139+AndrewKhukhrianskyi@users.noreply.github.com>
2021-09-06 13:14:43 +0300

    Items page (#34)
    Test fixing another

--- pages/items_page.rb
+++ pages/items_page.rb
@@ -1,13 +1,19 @@
 class ItemsPage < BasePage
   PAGE_TITLE = 'ITEMS'
 
+  # Buttons & fields
+	
   def quantity
-    browser.find_element(xpath:'//p[@id="quantity_wanted"]/input[@name ="qty"]')
+    browser.find_element(id:'quantity_wanted')
   end
   
   def quantity_enter(data)
 	quantity.clear
 	quantity.send_keys(data)
+  end
+  
+  def fullscr_exs?
+	!browser.find_elements(xpath: '//body/div[@class = "fancybox-wrap fancybox-desktop fancybox-type-image fancybox-opened"]').empty?
   end
   
   def asc_btn
@@ -22,64 +28,136 @@ class ItemsPage < BasePage
 	browser.find_element(id: 'bigpic')
   end
   
+  def wishlist
+	browser.find_element(id: 'wishlist_button')
+  end
+  
   def size
-	browser.find_element(id: 'group_1')
+	browser.find_element(xpath: '//select[@id = "group_1"]')
   end
   
   def size_sub
-	browser.find_element(xpath: '//select[@class="form-control attribute_select no-print"]/options[@title = "M"]')
+	browser.find_element(xpath: '//select[@id = "group_1"]/option[@value = "2"]')
+  end
+  
+  def apply_btn
+	browser.find_element(class: 'button-medium')
+  end
+  
+  def cont_btn
+	browser.find_element(class: 'exclusive-medium')
   end
   
   def cart_btn
-	browser.find_element(class: 'exclusive')
-  end
-  
-  def fullscr_photo
-	browser.find_element(class: 'fancybox-outer')
+	browser.find_element(id: 'add_to_cart')
   end
   
   def fullscr_nxt
-	browser.find_element(class: 'fancybox-nav fancybox-next')
+	browser.find_element(class: 'fancybox-next')
   end
   
   def fullscr_prev
-	browser.find_element(class: 'fancybox-nav fancybox-prev')
+	browser.find_element(class: 'fancybox-prev')
   end
   
   def fullscr_cls
-	browser.find_element(class: 'fancybox-item fancybox-close')
+	browser.find_element(class: 'fancybox-close')
   end
-  
-  def tweet
-	browser.find_element(class: 'btn btn-default btn-twitter')
-  end
-  
-  def penter
-	browser.find_element(class: 'fancybox-nav fancybox-prev')
-  end
-  
-  def facebook
-	browser.find_element(class: 'btn btn-default btn-facebook')
-  end
-  
-  def google
-	browser.find_element(class: 'btn btn-default btn-google-plus')
-  end
-  
-  def sub_photos
-	browser.find_element(class: 'thumbs_list_frame')
+ 
+  def abort_btn
+	browser.find_element(class: 'cross')
   end
   
   def view_scroll
 	browser.find_element(id: 'view_scroll_right')
   end 
   
-  def subphoto
-	browser.find_element(id: 'thumb_2')
+  def tweet
+	browser.find_element(class: 'btn-twitter')
+  end
+  
+  def penter
+	browser.find_element(class: 'btn-pinterest')
+  end
+  
+  def facebook
+	browser.find_element(class: 'btn-facebook')
+  end
+  
+  def google
+	browser.find_element(class: 'btn-google-plus')
   end
   
   def view_scroll_lft
 	browser.find_element(id: 'view_scroll_left')
   end
   
+  def friend_btn
+	browser.find_element(id: 'send_friend_button')
+  end
+  
+  def print_btn
+	browser.find_element(class: 'print')
+  end
+  
+  def friend_name
+	browser.find_element(id: 'friend_name')
+  end
+  
+  def friend_email
+	browser.find_element(id: 'friend_email')
+  end
+  
+  def send_btn
+	browser.find_element(id: 'sendEmail')
+  end
+  
+  def add_form(name, email)
+	friend_name.send_keys(name)
+	friend_email.send_keys(email)
+  end
+  
+  def ok_btn
+	browser.find_element(xpath: '//p[@class = "submit"]/input[@value = "OK"]')
+  end
+  
+  
+  
+  # Message boxes, Labels, active widgets 
+  
+  def btn_container
+	browser.find_element(class: 'button-container')
+  end
+  
+  def friend_form
+	browser.find_element(id: 'send_friend_form')
+  end
+  
+  def message
+	browser.find_element(class: 'fancybox-inner')
+  end
+  
+  def layer_cart
+	browser.find_element(class: 'clearfix')
+  end
+  
+  def item_alert
+	browser.find_element(class: 'fancybox-outer')
+  end
+ 
+  def amount
+	browser.find_element(class: 'unvisible')
+  end
+  
+  def fullscr
+	browser.find_element(class: 'fancybox-image')
+  end
+  
+  def sub_photos
+	browser.find_element(id: 'thumbs_list')
+  end
+  
+  def subphoto
+	browser.find_element(id: 'thumb_2')
+  end
 end
--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 
-RSpec.describe 'Items tab' do
-  search_page = ItemsPage.new
+RSpec.describe 'Items page' do
+  items_page = ItemsPage.new
 
   before(:all) do
     browser.get(PRODUCT)
@@ -11,6 +11,7 @@ RSpec.describe 'Items tab' do
   after(:all) { browser.close } 
   
   context 'UI checking' do
+	# UI Testing on the Items page
 	it 'verifies that quantities field is displayed' do
 		expect(items_page.quantity.displayed?).to be(true)
 	end
@@ -25,12 +26,12 @@ RSpec.describe 'Items tab' do
 			
 	end
 	
-	it 'verifies that photo is displayed' do
-		expect(items_page.photo.displayed?).to be(true)
+	it 'verifies that wishlist button is displayed' do
+		expect(items_page.wishlist.displayed?).to be(true)
 	end
 	
-	it 'verifies that size field is displayed' do
-		expect(items_page.size.displayed?).to be(true)
+	it 'verifies that photo is displayed' do
+		expect(items_page.photo.displayed?).to be(true)
 	end
 	
 	it 'verifies that size subfield is displayed' do
@@ -65,28 +66,42 @@ RSpec.describe 'Items tab' do
 	it 'verifies that scroll button is displayed' do
 		expect(items_page.view_scroll.displayed?).to be(true)
 	end
+	
+	it 'verifies that print button is displayed' do
+		expect(items_page.print_btn.displayed?).to be(true)
+	end
+	
+	it 'verifies that send to friend button is displayed' do
+		expect(items_page.friend_btn.displayed?).to be(true)
+	end
+	
+	it 'verifies that fullscreen is disabled' do
+		expect(items_page.fullscr_exs?).to be(false)
+	end
 
   end
   
   context 'when valid credentials' do
+	# Functional positive testing on the Items page
+	
     it 'verifies user can type numbers in the quantities field' do
 		data = 5
 		items_page.quantity_enter(data)
-		expect(items_page.quantity.text).to_i == data
+		expect(items_page.quantity.text) == data.to_s
     end
 	
 	it 'verifies that user can click on the increment button' do
 		data = 2
 		items_page.quantity_enter(data)
 		items_page.asc_btn.click
-		expect(items_page.quantity.text).to_i == data + 1		
+		expect(items_page.quantity.text) == (data + 1).to_s		
 	end
 	
 	it 'verifies that user can click on the decrement button' do
 		data = 2
 		items_page.quantity_enter(data)
 		items_page.desc_btn.click
-		expect(items_page.quantity.text).to_i == data - 1		
+		expect(items_page.quantity.text) == (data - 1).to_s		
 	end
 	
 	it 'verifies that user can choose size' do
@@ -95,52 +110,76 @@ RSpec.describe 'Items tab' do
 		expect(items_page.size.text) == 'M'
 	end
 	
-	it 'verifies that user can click on the photo and see photo in the fullscreen format' do
+	
+	it 'verifies that user can click on the photo on the subphotos' do
+		items_page.subphoto.click
+		expect(items_page.fullscr.displayed?).to be(true)
+	end
+	
+	it 'verifies that user can click on the photo and see photo in the fullscreen' do
+		items_page.fullscr_cls.click
 		items_page.photo.click
-		expect(items_page.fullscr_photo.displayed?).to be(true)
+		expect(items_page.fullscr.displayed?).to be(true)
 	end
 	
-	it 'verifies that user can click on the next photo in the fullscreen format' do
-		items_page.photo.click
-		items_page.fullscr_nxt
-		expect(items_page.fullscr_photo.displayed?).to be(true)
+	it 'verifies that user can click on the next photo in the fullscreen ' do
+		items_page.fullscr_nxt.click
+		expect(items_page.fullscr.displayed?).to be(true)
 	end
 	
-	it 'verifies that user can click on the previous photo in the fullscreen format' do
-		items_page.photo.click
-		items_page.fullscr_prev
-		expect(items_page.fullscr_photo.displayed?).to be(true)
+	it 'verifies that user can click on the previous photo in the fullscreen ' do
+		items_page.fullscr_prev.click
+		expect(items_page.fullscr.displayed?).to be(true)
 	end
 	
-	it 'verifies that user can close fullscreen format by using close button' do
-		items_page.photo.click
-		items_page.fullscr_cls
-		expect(items_page.fullscr_photo.displayed?).to be(false)
+	it 'verifies that unathorized user can not add item to the wishlist' do
+		items_page.fullscr_cls.click
+		items_page.wishlist.click
+		expect(items_page.item_alert.displayed?).to be(true)
 	end
 	
-	it 'verify that user can switch the subphotos by clicking on the scroll button' do
-		items_page.view_scroll.click
-		expect(items_page.subphoto.displayed?).to be(false)
+	it 'verifies that friend form is displayed' do
+		items_page.fullscr_cls.click
+		items_page.friend_btn.click
+		expect(items_page.friend_form.displayed?).to be(true)
 	end
 	
-	it 'verify that user can go back to the previous subphotos by clicking on the scroll button' do
-		items_page.view_scroll_lft.click
-		expect(items_page.subphoto.displayed?).to be(true)
+	it 'verifies that user can send info to the friend' do
+		name = 'Robb Stark'
+		email = 'RStark@gameofthrones.com'
+		items_page.add_form(name, email)
+		items_page.send_btn.click
+		expect(items_page.message.displayed?).to be(true)	
 	end
 	
-	it 'verify that user can click on the subphoto and see the choosed photo in the fullscreen format'
-		expect(items_page.fullscr_photo.displayed?).to be(true)
+	it 'verifies that information about product is displayed' do
+		items_page.ok_btn.click
+		items_page.cart_btn.click
+		expect(items_page.layer_cart.displayed?).to be(true)
 	end
 	
+	it 'verifies that user can abort order' do
+		items_page.abort_btn.click
+		expect(items_page.amount.text) == '(empty)'
+	end
 	
-  end
-  
-  context 'when invalid credentials' do
-	it 'verifies that quantity can not be negative or zero by clicking on the decrement button' do
+	it 'verifies that amount of items in the cart is switched' do
 		data = 1
-		items_page.quantity_enter(data)
-		items_page.desc_btn.click
-		expect(items_page.quantity.text).to_i == data	
+		items_page.cart_btn.click
+		items_page.cont_btn.click
+		expect(items_page.amount.text) == data.to_s + ' Product'
 	end
-
+	
+	it 'verifies that after clicking on the continue button user will be on the item page' do
+		items_page.cart_btn.click
+		items_page.cont_btn.click
+		expect(items_page.url_include?('controller=order')).to be(false)	
+	end
+	
+	it 'verifies that user can add item to the cart and redirect to the order page' do
+		items_page.cart_btn.click
+		items_page.apply_btn.click
+		expect(items_page.url_include?('controller=order')).to be(true)
+	end
+  end
 end
